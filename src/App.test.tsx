cea69528687531e13987c7b907bcf00a19b248f0
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-09-12 16:42:55 +0530

    Removed App Test
    pre-push removed (husky) as tests are not made yet, App.test.tsx is now depriciated

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,9 @@
+// This is an example test file for the App component.
+// All test files should have <component_name>.test.tsx as the file name.
+// This file is written using the vitest testing library.
+// The test file should test the component's functionality and not the implementation details.
+// The test file should be placed in the same directory as the component file.
+
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
